4eaf60738f2d1097d59764562643f0c07c256144
kishan.p <kishan.p@reportgarden.com>
2018-12-31 02:50:03 +0530

    question and assignment moddels are added have to structure the result and the user model

--- codehub/settings.py
+++ codehub/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    #config for codingcenter
+    'codingcenter.apps.CodingcenterConfig',
 ]
 
 MIDDLEWARE = [
--- codingcenter/views.py
+++ codingcenter/views.py
@@ -1,2 +1,33 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
+
+
+class AssignmentListView(APIView):
+    """
+    List View to list out all assignments
+    #get: return the list of assignemts
+    #post: accept the config for the assignemt
+    """
+    def get(self, request, format = None):
+        sample_data = [
+            {
+                "name":"Hack1",
+                "owner":"abhinav_dayal",
+                "questions_count": 30,
+                "status":"active",
+            },
+            {
+                "name": "Hack2",
+                "owner": "abhinav_dayal",
+                "questions_count": 30,
+                "status": "active",
+            }
+        ]
+        return Response(sample_data, status = status.HTTP_200_OK)
+
+    def post(self, request, format = None):
+
+        return Response()
