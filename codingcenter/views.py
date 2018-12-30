6ced838a29310252b647546fe585ecb7d8d53360
kishan.p <kishan.p@reportgarden.com>
2018-12-31 00:59:23 +0530

    added the requriments file to production

--- codingcenter/views.py
+++ codingcenter/views.py
@@ -1,2 +1,19 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+
+
+class AssignmentList(APIView):
+    """
+    List View to list out all assignments
+    #get: return the list of assignemts
+    #post: accept the config for the assignemt
+    """
+    def get(self, request, format = None):
+
+        return Response()
+
+    def post(self, request, format = None):
+
+        return Response()
