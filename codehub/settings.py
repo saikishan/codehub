3b183783be52794182f475267448637e18bcdc3c
saikishan <saikishan2008@gmail.com>
2019-01-10 11:50:11 +0530

    added the djangorest jwt support yet to update custom usermodel

--- codehub/settings.py
+++ codehub/settings.py
@@ -42,6 +42,20 @@ INSTALLED_APPS = [
     'codingcenter.apps.CodingcenterConfig',
 ]
 
+#config for restframework
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
