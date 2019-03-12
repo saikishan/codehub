542b26b635e6a60c3b24910ed3ccb12a2058646c
saikishan <kishan.p@reportgarden.com>
2019-03-12 23:11:38 +0530

    updated the gitignore

--- codehub/celery.py
+++ codehub/celery.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codehub.settings')
 
 app = Celery('codehub')
@@ -12,3 +13,5 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
